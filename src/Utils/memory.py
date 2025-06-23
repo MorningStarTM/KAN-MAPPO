11f2ef7f4350d34445d96bd904ebd3b832d8cf1c
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-06-23 13:40:15 +0530

    ppo memory created

---
+++ src/Utils/memory.py
@@ -0,0 +1,19 @@
+class RolloutBuffer:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.state_values = []
+        self.is_terminals = []
+    
+    def clear(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.state_values[:]
+        del self.is_terminals[:]
+
+    def __len__(self):
+        return len(self.rewards)
