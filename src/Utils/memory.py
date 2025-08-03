ec98073cb7609b6a66fe766697e9b9bd4dedeef2
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-08-03 19:46:33 +0530

    component removed

--- src/Utils/memory.py
+++ src/Utils/memory.py
@@ -2,7 +2,6 @@ class RolloutBuffer:
     def __init__(self):
         self.actions = []
         self.states = []
-        self.global_states = []
         self.logprobs = []
         self.rewards = []
         self.state_values = []
@@ -11,7 +10,6 @@ class RolloutBuffer:
     def clear(self):
         del self.actions[:]
         del self.states[:]
-        del self.global_states[:]
         del self.logprobs[:]
         del self.rewards[:]
         del self.state_values[:]
